c553337a59210093a4863e9333df4e5cfa332404
David Barron <90076120+webgeekbear@users.noreply.github.com>
2021-10-06 09:04:45 -0700

    Added basic questions to quiz.  Added icon for visibility.

---
+++ script.js
@@ -0,0 +1,29 @@
+var quiz = [
+    {
+        question: "How much wood would a wood chuck chuck?",
+        answers: [
+            {
+                correct: false,
+                display: "Unknown"
+            },
+            {
+                correct: true,
+                display: "If a wood chuck could chuck wood"
+            }
+        ]
+    },
+    {
+        "question": "Mares eat oats and does eat oats?",
+        "answers": [
+            {
+                correct: false,
+                display: "Unknown"
+            },
+            {
+                correct: true,
+                display: "and little lambs eat ivy"
+            }
+        ]
+    }
+];
+
